feecf653a44b9f7244f642776f44ccfeeb1e6692
daviferrarez1999 <daviferrarez@gmail.com>
2024-10-16 21:15:16 -0300

    Mudanças no express

--- modules/express.js
+++ modules/express.js
@@ -3,31 +3,80 @@ const UserModel = require('../src/models/user_model')
 
 const app = express()
 
-app.get('/home', (req, res) => {
-    res.contentType('application.html')
-    res.status(200).send("<h1>hello world</h1>");
+app.use(express.json())
+
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+
+app.use((req, res, next) => {
+    console.log(`Request Type: ${req.method}`)
+    console.log(`Content Type: ${req.headers["content-type"]}`);
+    console.log(`Date: ${new Date()}`);
+
+    next();
+})
+
+app.get('/views/users', async (req, res) => {
+    const users = await UserModel.find({})
+    
+    res.render('index', {users});
+})
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await UserModel.find({});
+
+        res.status(200).json(users)
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 });
 
-app.get('/users', (req, res) => {
-    const users = [
-        {
-            name: "John Doe",
-            email: "john@doe.com"
-        },
-        {
-            name: "Jane Doe",
-            email: "jane@doe.com"
-        },
-    ];
+app.get('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
 
-    res.status(200).json(users);
+        const user = await UserModel.findById(id);
+
+        return res.status(200).json(user)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+})
+
+app.post("/users", async (req, res) => {
+    try {
+        const user = await UserModel.create(req.body);
+
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
 });
 
-app.post("/users", (req, res) => {
-    const user = UserModel.create(req.body);
+app.patch('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        
+        const user = await UserModel.findByIdAndUpdate(id, req.body, {new: true})
 
-    res.status(201).json(user);
-});
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const user = await UserModel.findByIdAndDelete(id)
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+})
 
 const port = 8080;
 
